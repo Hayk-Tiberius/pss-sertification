8d8dbe612077da71bf5f7a4a41019e38283e4cf8
Hayk Marius <avrecij02@gmail.com>
2024-10-20 04:53:14 +0300

    Slider

--- src/Main.jsx
+++ src/Main.jsx
@@ -6,6 +6,7 @@ import video1 from "./components/video/slider1.mp4"
 import video2 from "./components/video/slider2.mp4"
 import video3 from "./components/video/slider3.mp4"
 import "./Slider.scss";
+import "./Main.scss";
 
 const Main = () => {
     const slides = [
@@ -24,6 +25,11 @@ const Main = () => {
             <Link to="Sertification.jsx">
             <div className="sertification__title">О сертификации</div>
             </Link>
+        </section>
+        <section className="main_applications">
+            <div className="main_applications_item">Менеджмент качества</div>
+            <div className="main_applications_item">Менеджмент безопасности</div>
+            <div className="main_applications_item">Экологический менеджмент</div>
         </section>
         </>
     )
--- src/Pages/Sertification.jsx
+++ src/Pages/Sertification.jsx
@@ -1,17 +1,52 @@
 import React from "react";
 import Header from "../tools/Header";
+import "./Sertification.scss"
+
+import poryadok_zhalob  from "../components/files/serification/poryadok_zhalob.pdf"
+import sertification_schema  from "../components/files/serification/sertification_schema.pdf"
+import politic_conf  from "../components/files/serification/politic_conf.pdf"
+import politic_non_involve  from "../components/files/serification/politic_non_invol.pdf"
+import politic_cancel  from "../components/files/serification/politic_cancel.pdf"
+import politic_accordance  from "../components/files/serification/politic_accordance.pdf"
+import politic_distance  from "../components/files/serification/politic_distance.pdf"
 
 
 const Sertification = () => {
     return(
         <>
         <Header />
-        НОРМАТИВНЫЕ ДОКУМЕНТЫ, ИСПОЛЬЗУЕМЫЕ ПРИ ВЫПОЛНЕНИИ РАБОТ ПО СЕРТИФИКАЦИИ СИСТЕМ МЕНЕДЖМЕНТА
-        ГОСТ Р ИСО 9001-2015 Системы менеджмента качества. Требования
-        ГОСТ Р ИСО 14001-2016 Системы экологического менеджмента. Требования и руководство по применению
-        ГОСТ Р ИСО 45001-2020. Системы менеджмента безопасности труда и охраны здоровья. Требования и руководство по применению.
-        ГОСТ Р ИСО/МЭК 17021-1-2017 Оценка соответствия. Требования к органам, проводящим аудит и сертификацию систем менеджмента
+        <span className="sertification_title">НОРМАТИВНЫЕ ДОКУМЕНТЫ, ИСПОЛЬЗУЕМЫЕ ПРИ ВЫПОЛНЕНИИ РАБОТ ПО СЕРТИФИКАЦИИ СИСТЕМ МЕНЕДЖМЕНТА</span>
+        <div className="sertification_list">
+            <li>ГОСТ Р ИСО 9001-2015 Системы менеджмента качества. Требования</li>
+            <li>ГОСТ Р ИСО 14001-2016 Системы экологического менеджмента. Требования и руководство по применению</li>
+            <li>ГОСТ Р ИСО 45001-2020. Системы менеджмента безопасности труда и охраны здоровья. Требования и руководство по применению.</li>
+            <li>ГОСТ Р ИСО/МЭК 17021-1-2017 Оценка соответствия. Требования к органам, проводящим аудит и сертификацию систем менеджмента</li>
+        </div>
 
+        <div className="files">
+            <div className="files_item">
+                <a href={poryadok_zhalob} onClick={(e) => { e.preventDefault(); window.open(poryadok_zhalob, '_blank'); }}>Скачать файл</a>
+            </div>
+            <div className="files_item">
+                <a href={sertification_schema}  onClick={(e) => { e.preventDefault(); window.open(sertification_schema, '_blank'); }}>Скачать файл</a>
+            </div>
+            <div className="files_item">
+                <a href={politic_conf}  onClick={(e) => { e.preventDefault(); window.open(politic_conf, '_blank'); }}>Скачать файл</a>
+            </div>
+            <div className="files_item">
+                <a href={politic_non_involve}  onClick={(e) => { e.preventDefault(); window.open(politic_non_involve, '_blank'); }}>Скачать файл</a>
+            </div>
+            <div className="files_item">
+                <a href={politic_cancel}  onClick={(e) => { e.preventDefault(); window.open(politic_cancel, '_blank'); }}>Скачать файл</a>
+            </div>
+            <div className="files_item">
+                <a href={politic_accordance}  onClick={(e) => { e.preventDefault(); window.open(politic_accordance, '_blank'); }}>Скачать файл</a>
+            </div>
+            <div className="files_item">
+                <a href={politic_distance}  onClick={(e) => { e.preventDefault(); window.open(politic_distance, '_blank'); }}>Скачать файл</a>
+            </div>
+        </div>
+        
         </>
     )
 }
