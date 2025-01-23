c33571a65d170748abe6716b98b22e93384066e9
Hayk Marius <avrecij02@gmail.com>
2025-01-23 04:53:45 +0300

    Structure

--- src/Pages/company pages/Partners.jsx
+++ src/Pages/company pages/Partners.jsx
@@ -1,13 +1,24 @@
 import React from "react"
 import Header from "../../tools/Header"
 import CompanyHeader from "./CompanyHeader"
-
+//SCSS В STRUCTURE
 
 const Partners = () => {
     return ( 
         <>
         <Header/>
         <CompanyHeader/>
+        <main>
+            <span>
+                Профессиональные объединения (партнерства):
+            </span>
+            <div className="partners__block">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            
+        </main>
         </>
     )
 }
--- src/Pages/company pages/Structure.jsx
+++ src/Pages/company pages/Structure.jsx
@@ -1,6 +1,8 @@
 import React from "react"
 import Header from "../../tools/Header"
 import CompanyHeader from "./CompanyHeader"
+import "../../Pages/company pages/Structure.scss"
+import photo1 from "../../components/img/structure/Structure.PNG"
 
 
 const Structure = () => {
@@ -8,6 +10,14 @@ const Structure = () => {
         <>
         <Header/>
         <CompanyHeader/>
+        <main>
+            <div>
+            <span>Организационная структура Органа по сертификации систем менеджмента качества ООО ПРОМСТРОЙ-Сертификация</span><br/>
+            <span>ОС – орган по сертификации систем менеджмента качества ООО ПРОМСТРОЙ-Сертификация</span><br/>
+            <img src={photo1} alt="" /><br/>
+            <span>* - комиссия формируется на период рассмотрения апелляции или жалобы</span>
+            </div>
+        </main>
         </>
     )
 }
