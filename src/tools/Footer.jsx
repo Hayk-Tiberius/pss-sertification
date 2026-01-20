8cf4c3dbf555aa687b13ab1a0f2dd104e629f5e7
Hayk Marius <avrecij02@gmail.com>
2026-01-20 20:14:22 +0300

    History and Info fixing

--- src/tools/Footer.jsx
+++ src/tools/Footer.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-import "../../src/Footer.scss"
-
-
+import "../../src/Footer.scss";
 
 const Footer = () => {
   return (
-    
     <footer className="footer">
       <div className="footer__content">
-        <p>© 1999 - 2025 ООО "ПРОМСТРОЙ – Сертификация"</p>
-        <p>Электронная почта: <a href="mailto:ps-s@ps-s.ru">ps-s@ps-s.ru</a></p>
+        <p>© 1999 - 2026 ООО "ПРОМСТРОЙ – Сертификация"</p>
+        <p>
+          Электронная почта: <a href="mailto:ps-s@ps-s.ru">ps-s@ps-s.ru</a>
+        </p>
       </div>
     </footer>
   );
