a0828a2461e9c8b3ddbc92c30fce4b3313660a02
Hayk Marius <avrecij02@gmail.com>
2026-01-27 23:56:09 +0300

    Accordion and Footer fixing

--- src/tools/Header.jsx
+++ src/tools/Header.jsx
@@ -1,35 +1,37 @@
-import React from "react"
-import logo from "../components/img/pss_logo.svg"
-import { Link } from 'react-router-dom'
-import "../../src/Header.scss"
-
+import React from "react";
+import logo from "../components/img/pss_logo.svg";
+import { Link } from "react-router-dom";
+import "../../src/Header.scss";
 
 const Header = () => {
-    return (   
+  return (
     <header className="header">
-        <div className="logo">
-            <Link to="/">
-            <img src={logo} alt="logo" />
-            <span className="logo__title">ПРОМСТРОЙ-</span>
-            <span className="logo__title">Сертификация</span>
-            </Link>
-        </div>
-        <div className="menu">
-            <Link to="/Company">
-                <span className="menu__hover">О компании</span>
-            </Link>
-            <Link to="/Info">
-                <span className="menu__hover">Информация</span>
-            </Link>
-            <Link to="/Applications">
-                <span className="menu__hover">Заявки на услуги</span>
-            </Link>
-            <Link to="/Contact">
-                <span className="menu__hover">Контакты</span>
-            </Link>
-        </div>
+      <div className="logo">
+        <Link to="/">
+          <img src={logo} alt="logo" />
+          <span className="logo__title">
+            ПРОМСТРОЙ-
+            <br />
+            сертификации
+          </span>
+        </Link>
+      </div>
+      <div className="menu">
+        <Link to="/Company">
+          <span className="menu__hover">О компании</span>
+        </Link>
+        <Link to="/Info">
+          <span className="menu__hover">Информация</span>
+        </Link>
+        <Link to="/Applications">
+          <span className="menu__hover">Заявки на услуги</span>
+        </Link>
+        <Link to="/Contact">
+          <span className="menu__hover">Контакты</span>
+        </Link>
+      </div>
     </header>
-    )
-}
+  );
+};
 
-export default Header
+export default Header;
